1fdfef28eaa38e91d1c16a0d26f3dc0cdb09e181
FazleyBin-Mahbub <ariyanrabby94@gmail.com>
2023-06-23 19:19:42 +0600

    controller added

--- lib/controller/quiz_controller.dart
+++ lib/controller/quiz_controller.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:quizzify/models/que_model.dart';
+import 'package:quizzify/services/api_services.dart';
 
 class QuizController extends ChangeNotifier {
+  QuestionModel questionModel = QuestionModel();
   int _seconds = 60;
   final int _currentQueIndex = 0;
+  bool loading = false;
   late final Timer? _timer;
 
-
   int get seconds => _seconds;
+
   int get currentQueIndex => _currentQueIndex;
 
   Timer get timer => _timer!;
@@ -24,7 +28,14 @@ class QuizController extends ChangeNotifier {
       }
     });
   }
-// start timer end
-// using startTimer
 
+// start timer end
+
+// get question data
+  Future<void> getQuestions() async {
+    loading = true;
+    questionModel = await ApiServices().getQuestions();
+    loading = false;
+    notifyListeners();
+  }
 }
--- lib/views/quiz/quiz_screen.dart
+++ lib/views/quiz/quiz_screen.dart
@@ -19,7 +19,9 @@ class _QuizScreenState extends State<QuizScreen> {
     // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      Provider.of<QuizController>(context, listen: false).startTimer();
+      final model = Provider.of<QuizController>(context, listen: false);
+      model.startTimer();
+      model.getQuestions();
     });
   }
 
@@ -43,144 +45,154 @@ class _QuizScreenState extends State<QuizScreen> {
               ],
             ),
           ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      border: Border.all(
-                        color: AppColors.lightGrey,
-                        width: 2,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(
-                        CupertinoIcons.xmark,
-                        size: 28,
-                        color: Colors.white,
-                      ),
+          child: controller.loading
+              ? const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      AppColors.darkBlue,
                     ),
                   ),
-                  Stack(
-                    alignment: Alignment.center,
+                )
+              : Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        controller.seconds.toString(),
-                        style: AppStyles.textStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.w900,
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          border: Border.all(
+                            color: AppColors.lightGrey,
+                            width: 2,
+                          ),
+                          shape: BoxShape.circle,
                         ),
-                      ),
-                      SizedBox(
-                        height: size.height * 0.1,
-                        width: size.width * 0.2,
-                        child: CircularProgressIndicator(
-                          value: controller.seconds / 60,
-                          valueColor: const AlwaysStoppedAnimation<Color>(
-                            Colors.white,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(
+                            CupertinoIcons.xmark,
+                            size: 28,
+                            color: Colors.white,
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      border: Border.all(
-                        color: AppColors.lightGrey,
-                        width: 2,
                       ),
-                    ),
-                    child: TextButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(
-                        CupertinoIcons.heart_fill,
-                        color: Colors.white,
-                        size: 18,
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Text(
+                            controller.seconds.toString(),
+                            style: AppStyles.textStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          SizedBox(
+                            height: size.height * 0.1,
+                            width: size.width * 0.2,
+                            child: CircularProgressIndicator(
+                              value: controller.seconds / 60,
+                              valueColor:
+                                  const AlwaysStoppedAnimation<Color>(
+                                Colors.white,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                      label: Text(
-                        "Like",
-                        style: AppStyles.textStyle(
-                          color: Colors.white,
-                          fontSize: 14,
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(
+                            color: AppColors.lightGrey,
+                            width: 2,
+                          ),
+                        ),
+                        child: TextButton.icon(
+                          onPressed: () {},
+                          icon: const Icon(
+                            CupertinoIcons.heart_fill,
+                            color: Colors.white,
+                            size: 18,
+                          ),
+                          label: Text(
+                            "Like",
+                            style: AppStyles.textStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
                         ),
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Image.asset(
+                    AppAssets.ideas,
+                    width: size.width * 0.5,
+                  ),
+                  const SizedBox(height: 25),
+                  // questions
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Question ${controller.currentQueIndex + 1} of 40",
+                      style: AppStyles.textStyle(
+                        color: AppColors.lightGrey,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "What is Rabby's Wife Name?",
+                    style: AppStyles.textStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // options
+                  Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: 4,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 12),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              minimumSize: Size(
+                                size.width - 100,
+                                size.height / 15,
+                              ),
+                              backgroundColor: Colors.white,
+                            ),
+                            child: Text(
+                              "Hasina",
+                              style: AppStyles.textStyle(
+                                color: AppColors.blue,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ),
                 ],
               ),
-              const SizedBox(height: 20),
-              Image.asset(
-                AppAssets.ideas,
-                width: size.width * 0.5,
-              ),
-              const SizedBox(height: 25),
-              // questions
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Question ${controller.currentQueIndex + 1} of 40",
-                  style: AppStyles.textStyle(
-                    color: AppColors.lightGrey,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w900,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                "What is Rabby's Wife Name?",
-                style: AppStyles.textStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              const SizedBox(height: 20),
-              // options
-              Expanded(
-                child: ListView.builder(
-                  physics: const BouncingScrollPhysics(),
-                  itemCount: 4,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 12),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                          minimumSize: Size(
-                            size.width - 100,
-                            size.height / 15,
-                          ),
-                          backgroundColor: Colors.white,
-                        ),
-                        child: Text(
-                          "Hasina",
-                          style: AppStyles.textStyle(
-                            color: AppColors.blue,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
